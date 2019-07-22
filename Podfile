f40ec4a0e717a78cec0832323b839fd5e12deb49
胡伟东 <duoyi@duoyis-macbook-air.local>
2019-07-22 20:53:08 +0800

    练习项目

---
+++ Podfile
@@ -0,0 +1,11 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '9.0'
+
+target 'myLookupTableTest' do
+    
+# 第三方框架
+  pod 'GPUImage'
+  
+  
+end
--- myLookupTableTest/ViewController.m
+++ myLookupTableTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <GPUImage.h>
 
 @interface ViewController ()
 
@@ -17,7 +18,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGRect screen = [UIScreen mainScreen].bounds;
+    
+    UIImage *image = [UIImage imageNamed:@"weixin_popover"];
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, image.size.width, image.size.height)];
+    imageView.center = CGPointMake(screen.size.width/2.0, screen.size.height/2.0);
+    [self.view addSubview:imageView];
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        UIImage *filterImage = [self applyFIlter:image];
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            imageView.image = filterImage;
+        });
+    });
 }
 
+- (UIImage *)applyFIlter:(UIImage *)originalImg {
+    UIImage *inputImage = originalImg;
+    UIImage *outputImage = nil;
+    GPUImagePicture *stillImageSource = [[GPUImagePicture alloc] initWithImage:inputImage];
+    //添加滤镜
+    GPUImageLookupFilter *lookUpFilter = [[GPUImageLookupFilter alloc] init];
+    //导入之前保存的NewLookupTable.png文件
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"155131625201709141958art38978/892801501567939852359ba6ee4d4ad8" ofType:@"png"];
+    UIImage *image = [UIImage imageWithContentsOfFile:path];
+    GPUImagePicture *lookupImg = [[GPUImagePicture alloc] initWithImage:image];
+    [lookupImg addTarget:lookUpFilter atTextureLocation:1];
+    [stillImageSource addTarget:lookUpFilter atTextureLocation:0];
+    [lookUpFilter useNextFrameForImageCapture];
+    if([lookupImg processImageWithCompletionHandler:nil] && [stillImageSource processImageWithCompletionHandler:nil]) {
+        outputImage = [lookUpFilter imageFromCurrentFramebuffer];
+    }
+    
+    return outputImage;
+}
 
 @end
